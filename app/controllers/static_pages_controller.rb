053f2e755d4a0d5a414ef2a22b9d8b5d657d9a01
TheFSilver <dasylva.f@gmail.com>
2018-08-02 19:05:38 +0200

    Final update du code avant ajout front-end

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,31 @@
 class StaticPagesController < ApplicationController
+  before_action :logged_in_user, only: [:secret]
+
   def home
   end
 
   def secret
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password,
+                                 :password_confirmation)
+  end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to("/") unless current_user?(@user)
   end
 end
